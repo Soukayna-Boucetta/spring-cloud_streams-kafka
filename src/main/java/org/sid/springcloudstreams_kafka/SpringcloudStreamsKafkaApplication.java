bd43424360a5319d8bd62a6529c45221dc9a0811
Soukayna-Boucetta <soukaboustta1999@gmail.com>
2022-06-03 17:47:55 +0100

    Initial Commit

---
+++ src/main/java/org/sid/springcloudstreams_kafka/SpringcloudStreamsKafkaApplication.java
@@ -0,0 +1,14 @@
+package org.sid.springcloudstreams_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudStreamsKafkaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringcloudStreamsKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/springcloudstreams_kafka/SpringcloudStreamsKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.springcloudstreams_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudStreamsKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
